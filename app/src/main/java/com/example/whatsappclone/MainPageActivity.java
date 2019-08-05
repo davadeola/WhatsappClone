de3c72032d88112b228bdc60d0535764e207bf05
davadeola <adeoladavid15@gmail.com>
2019-08-05 21:02:32 +0300

    Added permissions for reading and writing contacts

--- app/src/main/java/com/example/whatsappclone/MainPageActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainPageActivity.java
@@ -1,7 +1,10 @@
 package com.example.whatsappclone;
 
+import android.Manifest;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,5 +40,16 @@ public class MainPageActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), FindUserActivity.class));
             }
         });
+
+        getPermissions();
+
+    }
+
+    private void getPermissions() {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 1);
+        }
     }
 }
